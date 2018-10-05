07cf599f1de9a44d4c99025c8f3b6d61e345f0d3
Jay Toler <jay.toler@hotmail.com>
2018-10-05 14:04:09 -0400

    Update xor.py

--- Set1/xor.py
+++ Set1/xor.py
@@ -2,4 +2,7 @@ def xor(hex1, hex2):
     """
        MISSING DESCRIPTION
     """
-    pass
+    answer = hex(int(hex1,16) ^ int(hex2,16))
+    answer = answer.replace('L', '').replace('0x', '')
+    answer = answer.zfill(len(hex1))
+    return answer
