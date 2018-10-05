328f312c449618514f0ff24c4ec61faeed712335
Jay Toler <jay.toler@hotmail.com>
2018-10-05 14:04:28 -0400

    Update repeating_key_xor.py

--- Set1/repeating_key_xor.py
+++ Set1/repeating_key_xor.py
@@ -2,4 +2,13 @@ def repeating_key_xor(string, key):
     """
        MISSING DESCRIPTION
     """
-    pass
+    multiply_factor = len(string)/len(key)
+    extended_key = key*multiply_factor
+    difference = len(string) - len(extended_key)
+    if difference > 0:
+        remaining = key[:difference]
+        extended_key += remaining
+    elif difference < 0:
+        extended_key = extended_key[:difference]
+    answer = xor(string, extended_key)
+    return answer
